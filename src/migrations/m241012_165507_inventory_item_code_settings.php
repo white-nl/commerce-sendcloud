2bc5df01f658895e7ae9d080a0b162a309e446c3
white-lukas <lukas@white.nl>
2024-10-17 17:08:36 +0200

    fix migration logic

--- src/migrations/m241012_165507_inventory_item_code_settings.php
+++ src/migrations/m241012_165507_inventory_item_code_settings.php
@@ -15,13 +15,12 @@ class m241012_165507_inventory_item_code_settings extends Migration
      */
     public function safeUp(): bool
     {
-        // Place migration code here...
         $projectConfig = Craft::$app->getProjectConfig();
 
         $hsCodeFieldHandle = $projectConfig->get('plugins.commerce-sendcloud.settings.hsCodeFieldHandle') ?? null;
         $originCountryFieldHandle = $projectConfig->get('plugins.commerce-sendcloud.settings.originCountryFieldHandle') ?? null;
         if ($hsCodeFieldHandle !== null || $originCountryFieldHandle !== null) {
-            $projectConfig->set('plugins.commerce-sendcloud.settings.useInventoryItemCodes', true);
+            $projectConfig->set('plugins.commerce-sendcloud.settings.useInventoryItemCodes', false);
         }
         return true;
     }
