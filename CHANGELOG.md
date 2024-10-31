dac4f130f089c3da3a7b8dcddac849efbfa1345f
white-lukas <lukas@white.nl>
2024-10-31 13:41:40 +0100

    fixed #24

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Release Notes for Craft Sendcloud Plugin
 
+## 4.0.1 - 2024-10-31
+
+### Fixed
+
+ - Fix weight calculation when weightUnit is grams ([#24](https://github.com/white-nl/commerce-sendcloud/issues/24))
+
 ## 4.0.0 - 2024-09-23
 
 ### Added
--- src/services/ParcelItems.php
+++ src/services/ParcelItems.php
@@ -64,7 +64,7 @@ class ParcelItems extends Component
 
         $weightUnit = Plugin::getInstance()->getSettings()->weightUnits;
         return match ($weightUnit) {
-            'g' => $weight * 1000,
+            'g' => $weight / 1000,
             'lb' => $weight * 0.453,
             default => $weight,
         };
